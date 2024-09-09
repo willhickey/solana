13646a05a399ea6358d6f97ef44c06ea5173ba7b
WillHickey <will.hickey@anza.xyz>
2024-09-09 16:28:38 -0500

    Add debugging echos

--- docs/build.sh
+++ docs/build.sh
@@ -7,15 +7,17 @@ cd "$(dirname "$0")"
 source ../ci/env.sh
 
 # shellcheck source=ci/rust-version.sh
+echo "build.sh starting build cli usage"
 source ../ci/rust-version.sh
 ../ci/docker-run-default-image.sh docs/build-cli-usage.sh
+echo "build.sh starting convert ascii to svg"
 ../ci/docker-run-default-image.sh docs/convert-ascii-to-svg.sh
 ./set-solana-release-tag.sh
-
+echo "build.sh starting npm run build"
 # Build from /src into /build
 npm run build
 echo $?
-
+echo "build.sh checking if publish"
 # Publish only from merge commits and beta release tags
 if [[ -n $CI ]]; then
   if [[ -z $CI_PULL_REQUEST ]]; then
